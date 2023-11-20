496aa999b889d75fdf782cb2557ae4d28cda6ca6
Sarju S <sarju.s@sjcetpalai.ac.in>
2023-11-20 14:49:36 +0530

    Create FileReaderDemo.java

---
+++ File/FileReaderDemo.java
@@ -0,0 +1,38 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderDemo {
+
+    public static void main(String[] args) {
+        // Array to store the read characters
+        char[] data = new char[29];
+
+        // FileReader object to read from a file
+        FileReader input = null;
+
+        try {
+            // Create a FileReader for the specified file path ("outputfile.txt")
+            input = new FileReader("outputfile.txt");
+
+            // Read characters from the file into the char array
+            int bytesRead = input.read(data);
+
+            // Print the read data (assuming it is text)
+            System.out.println(data);
+
+        } catch (IOException e) {
+            // Handle IO exceptions (e.g., file not found, permissions issue)
+            e.printStackTrace();
+        } finally {
+            try {
+                // Close the FileReader to release system resources
+                if (input != null) {
+                    input.close();
+                }
+            } catch (IOException e) {
+                // Handle any exception that occurs during closing resources
+                e.printStackTrace();
+            }
+        }
+    }
+}
